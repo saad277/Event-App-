c2f186cc35947097120da11bcc0830329cfa7dc3
Saad Sheikh <saadsheikh27@gmail.com>
2020-10-04 13:30:37 +0500

    details 2

--- src/Screens/Planner/EventDetails.js
+++ src/Screens/Planner/EventDetails.js
@@ -5,8 +5,11 @@ import { View, Text, StyleSheet, ScrollView, RefreshControl, FlatList, Touchable
 
 import Material from 'react-native-vector-icons/MaterialIcons'
 
+import FontAwesome from 'react-native-vector-icons/FontAwesome5'
 
+import { Tile } from 'react-native-elements';
 
+import { Container, Header, Content, Button, Icon, Item, Accordion } from 'native-base';
 
 const EventDetails = ({ navigation }) => {
 
@@ -16,12 +19,18 @@ const EventDetails = ({ navigation }) => {
 
 
     return (
-        <View style={styles.container}>
-            <Image source={{ uri: item.picture }} style={styles.image} />
+        <ScrollView style={styles.container}>
+            {/* <Image source={{ uri: item.picture }} style={styles.image} /> */}
+            <Tile
+                imageSrc={{ uri: item.picture }}
+                // title={item.name}
+                featured
+            //caption="Some Caption Text"
+            />
 
             <View style={styles.secondContainer}>
 
-                <View style={{ flexDirection: "row", borderBottomColor: "black", borderBottomWidth: 0.6, marginHorizontal: 15, paddingVertical: 10 }}>
+                <View style={styles.rowContainer}>
 
                     <View style={{ flexDirection: "column", flex: 1 }}>
 
@@ -39,16 +48,100 @@ const EventDetails = ({ navigation }) => {
 
                 <View style={{ flexDirection: "row", marginHorizontal: 15, marginTop: 30 }}>
 
-                    <Text style={styles.header}>$ {item.price}</Text>
+                    <Text style={[styles.header, { fontWeight: "900", flex: 1, marginLeft: 20 }]}>$ {item.price}</Text>
 
-                 
-                   
+                    <View style={{ justifyContent: "flex-end", backgroundColor: "red" }}>
+
+                        <Button iconLeft primary success style={{ justifyContent: "center", paddingHorizontal: 10, color: "#009387" }}>
+                            <Icon name='people' />
+                            <Text style={{ marginLeft: 10, fontSize: 16 }}>{item.members.length}</Text>
+                        </Button>
+
+                    </View>
+
+
+                </View >
+
+                <View style={[styles.rowContainer, { marginTop: 20 }]}>
+
+
+                    <View style={{ flexDirection: "column", flex: 1, }}>
+                        <Text style={styles.label}>City</Text>
+                        <Text style={styles.text}>{item.city}</Text>
+                    </View>
+
+                    <View style={{ flexDirection: "column", justifyContent: "flex-end", flex: 1 }}>
+                        <Text style={styles.label}>Country</Text>
+                        <Text style={styles.text}>{item.country}</Text>
+                    </View>
+
 
                 </View>
 
+
+                <View style={[styles.rowContainer, { marginTop: 20, borderBottomColor: "white", borderBottomWidth: 0, }]}>
+
+
+                    <View style={{ flexDirection: "column", flex: 1, }}>
+                        <Text style={styles.label}>From</Text>
+                        <Text style={styles.text}>{item.fromDate.slice(0, 10)}</Text>
+                    </View >
+
+
+
+                    <View style={{ flexDirection: "column", justifyContent: "flex-end", flex: 1, marginLeft: 20 }}>
+                        <Text style={styles.label}>To</Text>
+                        <Text style={styles.text}>{item.toDate.slice(0, 10)}</Text>
+                    </View>
+
+                    <View style={{ flexDirection: "column", }}>
+                        <FontAwesome size={30} color={"#009387"} name={"calendar"} />
+                    </View>
+
+                </View>
+
+                <View style={[styles.rowContainer, { marginTop: 20, }]}>
+
+
+                    <View style={{ flexDirection: "column", flex: 1, }}>
+                        <Text style={styles.label}>From</Text>
+                        <Text style={styles.text}>{item.fromDate.slice(11, 16)}</Text>
+                    </View >
+
+
+
+                    <View style={{ flexDirection: "column", justifyContent: "flex-end", flex: 1, marginLeft: 20 }}>
+                        <Text style={styles.label}>To</Text>
+                        <Text style={styles.text}>{item.toDate.slice(11, 16)}</Text>
+                    </View>
+
+                    <View style={{ flexDirection: "column", }}>
+                        <FontAwesome size={30} color={"#009387"} name={"clock"} />
+                    </View>
+
+                </View>
+
+                <View style={[styles.rowContainer,]}>
+
+                    <Content style={{ marginVertical: 20 }} >
+                        <Accordion
+                            dataArray={[{ title: "Description", content: item.description }]}
+                            expandedIcon="remove"
+                            expanded={1}
+                            iconStyle={{ color: "#009387" }}
+                            expandedIconStyle={{ color: "#009387", fontWeight: "bold" }}
+                            headerStyle={{ backgroundColor: "white" }}
+                            contentStyle={{ backgroundColor: "#ddecf8" }}
+
+                        />
+                    </Content>
+
+                </View>
+
+
             </View>
 
-        </View>
+        </ScrollView >
     )
 
 
@@ -63,7 +156,7 @@ const styles = StyleSheet.create({
     },
     image: {
         width: "100%",
-        height: 340,
+        height: 280,
         flex: 2
     },
     secondContainer: {
@@ -79,13 +172,13 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: "bold",
         textTransform: "capitalize",
-        marginLeft: 20,
+
     },
     sub: {
         fontSize: 18,
         marginTop: 5,
         color: "gray",
-        marginLeft: 20,
+
         fontWeight: "700"
 
     },
@@ -94,6 +187,26 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         flex: 1,
         justifyContent: "flex-end",
+
+    },
+    label: {
+
+        fontSize: 20,
+        color: "#009387"
+    },
+    text: {
+
+        fontSize: 16,
+        fontWeight: "700"
+    },
+    rowContainer: {
+
+        flexDirection: "row",
+        borderBottomColor: "black",
+        borderBottomWidth: 0.6,
+        marginHorizontal: 15,
+        paddingVertical: 10,
+        paddingLeft: 20
 
     }
 
